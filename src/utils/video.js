1b5e3b41050357ee70e76efc163075b6566d25cc
Wesley Moses <wesmoses@gmail.com>
2024-02-21 17:22:35 +0200

    try catch

--- src/utils/video.js
+++ src/utils/video.js
@@ -1,14 +1,18 @@
 export const supportsHEVCAlpha = () => {
-  const navigator = window.navigator;
-  const ua = navigator.userAgent.toLowerCase();
-  const hasMediaCapabilities = !!(
-    navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo
-  );
+  try {
+    const navigator = window.navigator;
+    const ua = navigator.userAgent.toLowerCase();
+    const hasMediaCapabilities = !!(
+      navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo
+    );
 
-  const isSafari =
-    ua.indexOf("safari") != -1 &&
-    !(ua.indexOf("chrome") != -1) &&
-    ua.indexOf("version/") != -1;
+    const isSafari =
+      ua.indexOf("safari") != -1 &&
+      !(ua.indexOf("chrome") != -1) &&
+      ua.indexOf("version/") != -1;
 
-  return isSafari && hasMediaCapabilities;
+    return isSafari && hasMediaCapabilities;
+  } catch {
+    return false;
+  }
 };
