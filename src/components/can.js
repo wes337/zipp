55c36adf80c8bd912978bfa5fd5880ae365621c9
Wesley Moses <wesmoses@gmail.com>
2024-02-21 17:17:47 +0200

    smaller can vid

--- src/components/can.js
+++ src/components/can.js
@@ -1,13 +1,18 @@
+"use client";
+
 import styles from "@/styles/can.module.scss";
+import { supportsHEVCAlpha } from "@/utils/video";
 
 export default function Can() {
+  const can = supportsHEVCAlpha() ? "/videos/can.mov" : "/videos/can.webm";
+
   return (
     <div className={styles.can}>
       <video autoPlay playsInline muted loop>
-        <source src="/videos/can.webm" />
+        <source src={can} />
       </video>
       <video className={styles.background} autoPlay playsInline muted loop>
-        <source src="/videos/can.webm" />
+        <source src={can} />
       </video>
     </div>
   );
