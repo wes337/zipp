62df052ec22e3091197ab270a9bbc0e6a98512a5
wes337 <wesmoses@gmail.com>
2024-02-21 23:32:41 +0200

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,10 @@ export const metadata = {
   description:
     "Naturally flavored energy drink. The future of the modern American beverage!",
   image: "https://zipp-coral.vercel.app/images/poster.png",
+  metadataBase: new URL("https://zipp-coral.vercel.app"),
+  openGraph: {
+    images: "/images/poster.png",
+  },
 };
 
 export default function RootLayout({ children }) {
