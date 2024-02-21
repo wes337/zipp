fd12cc1935b277554a3c14b083ec4770ffadde7b
wes337 <wesmoses@gmail.com>
2024-02-21 17:44:47 +0200

    can as mp4

--- src/components/can.js
+++ src/components/can.js
@@ -1,18 +1,21 @@
 "use client";
 
-import styles from "@/styles/can.module.scss";
 import { supportsHEVCAlpha } from "@/utils/video";
+import styles from "@/styles/can.module.scss";
 
 export default function Can() {
-  const can = supportsHEVCAlpha() ? "/videos/can.mov" : "/videos/can.webm";
-
   return (
     <div className={styles.can}>
       <video autoPlay playsInline muted loop>
-        <source src={can} />
+        <source
+          src={supportsHEVCAlpha() ? "/videos/can.mp4" : "/videos/can.webm"}
+        />
       </video>
       <video className={styles.background} autoPlay playsInline muted loop>
-        <source src={can} />
+        <source
+          src={supportsHEVCAlpha() ? "/videos/can.mp4" : "/videos/can.webm"}
+          type={supportsHEVCAlpha() ? "video/mp4;codecs=hvc1" : "video/webm"}
+        />
       </video>
     </div>
   );
--- src/utils/video.js
+++ src/utils/video.js
@@ -1,18 +1,18 @@
 export const supportsHEVCAlpha = () => {
-  try {
-    const navigator = window.navigator;
-    const ua = navigator.userAgent.toLowerCase();
-    const hasMediaCapabilities = !!(
-      navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo
-    );
-
-    const isSafari =
-      ua.indexOf("safari") != -1 &&
-      !(ua.indexOf("chrome") != -1) &&
-      ua.indexOf("version/") != -1;
-
-    return isSafari && hasMediaCapabilities;
-  } catch {
-    return false;
+  if (typeof window === "undefined") {
+    return;
   }
+
+  const navigator = window.navigator;
+  const ua = navigator.userAgent.toLowerCase();
+  const hasMediaCapabilities = !!(
+    navigator.mediaCapabilities && navigator.mediaCapabilities.decodingInfo
+  );
+
+  const isSafari =
+    ua.indexOf("safari") != -1 &&
+    !(ua.indexOf("chrome") != -1) &&
+    ua.indexOf("version/") != -1;
+
+  return isSafari && hasMediaCapabilities;
 };
