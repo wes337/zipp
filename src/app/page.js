14cf08d8260fb0e46c3ad31d9e9b2abeeb5a7769
wes337 <wesmoses@gmail.com>
2024-02-21 23:24:31 +0200

    hotline

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import { IconShoppingCart } from "@tabler/icons-react";
 import TopBar from "@/components/top-bar";
 import Can from "@/components/can";
 import Panel from "@/components/panel";
-import styles from "@/styles/home.module.scss";
 import Button from "@/components/button";
-import { IconShoppingCart } from "@tabler/icons-react";
+import styles from "@/styles/home.module.scss";
 
 const LEAD_TEXT = `Zipp™ contains highly delicious additives which stim-u-late your drinking experience and send your mind and taste buds rocketing through the roof! Your parents or landlord will need to hire a live-in contractor to do repairs for all of the roof holes you'll make after every sip! Zipp™ is that sicko nasty green gack that you can actually be proud to drink! Zipp's cracked-out research team has created a legendary formula that's super-richin flavor! Zipp™ is the future of the modern American beverage! Bust into a Zipp™ right this second or it'll be too late!`;
 
@@ -21,6 +21,9 @@ export default function Home() {
         <Button>
           <IconShoppingCart stroke={4} size={40} /> Buy
         </Button>
+        <div className={styles.hotline}>
+          <img src="/images/hotline.png" alt="" />
+        </div>
       </div>
     </div>
   );
--- src/components/can.js
+++ src/components/can.js
@@ -18,10 +18,10 @@ export default function Can() {
   return (
     <div className={styles.can}>
       <video autoPlay playsInline muted loop>
-        <source src={"/videos/can.webm"} type={"video/webm"} />
+        <source src={"/videos/can.webm"} type="video/webm" />
       </video>
       <video className={styles.background} autoPlay playsInline muted loop>
-        <source src={"/videos/can.webm"} type={"video/webm"} />
+        <source src={"/videos/can.webm"} type="video/webm" />
       </video>
     </div>
   );
